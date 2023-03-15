bc9c242b419d8f928c41a1bd50cd38d42d51ee03
Weerayut Namwongsa <weerayut6046@gmail.com>
2023-03-15 11:38:48 +0700

    update

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETLIVE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        // https://localhost:7000/api/company
+        [Route("")]
+        [HttpGet]
+        public IActionResult Hell() 
+        {
+            return Ok(new { message = "Hello .NET" });
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASPNETLIVE.Models
+{
+    public class Category
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryId { get; set; }
+
+        //[Column(TypeName = "ntext")] //ชนิดของข้อมูล
+        [Required(ErrorMessage = "ชื่อประเภทสินค้า ห้ามว่าง")]
+        public string CategoryName { get; set; } = null!;
+        public bool IsActive { get; set; } = true;
+    }
+}
